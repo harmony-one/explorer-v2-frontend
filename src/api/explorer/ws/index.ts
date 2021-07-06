427d7f5a797f29e27b3dbfda5536c4a80f717082
vpcodebase <84332922+vpcodebase@users.noreply.github.com>
2021-07-06 17:12:14 +0300

    set REACT_APP_PROD_ADDRESS for sockets (#12)
    * fix: search bar fix if number input, add badge (#2)
    
    * fix: search bar fix if number input, add badge
    for type in search bar, horfix for direct link in inventory page
    
    * fix: theme mode blink page
    
    * fix: write boolean in tx page
    
    * fix: styles for contract tab in address page
    
    * fix: remove btn
    
    * fix: env, results per page go to first page
    in grid. fix path for REACT_APP_EXPLORER_V1_API_URL
    
    * Update .env.example
    
    * fix: improve
    
    * fix: stable .env
    
    * fix: improve tx
    
    * fix: no update grid if tab not changed
    
    * fix: revert .env file
    
    * fix: add .env.development to gitignore
    
    * env var description
    
    * Update .env
    
    * fix: set to REACT_APP_PROD_ADDRESS socket
    
    Co-authored-by: potvik <57394565+potvik@users.noreply.github.com>
    Co-authored-by: Hype <hypnagonia@gmail.com>

--- src/api/explorer/ws/index.ts
+++ src/api/explorer/ws/index.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
  
-const socket = io(process.env.REACT_APP_DEV_ADDRESS as string, {
+const socket = io(process.env.REACT_APP_PROD_ADDRESS as string, {
   transports: ["websocket"],
 });
 
