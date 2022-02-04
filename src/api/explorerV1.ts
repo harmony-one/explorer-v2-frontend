46c8306150c9ca1d773946bc5b686a0525b33bd6
Victa Kwok Wai Phu <44614771+victaphu@users.noreply.github.com>
2022-02-04 19:13:24 +0800

    Fixed large files issue, and added support for multi-file uploads (#125)
    * update: added multi-file support
    
    * update: fixed display issue with single vs multi-file
    
    * update: merge from latest changes in explorer
    
    * update: .env file for testing purpose
    
    * update: fixed issue with language parameter supplying wrong values to backend service
    
    * update: added support for proxy
    
    * update: fixed env for testing
    
    * update: enabled contract verification on front end and added metamask button for proxy writes
    
    * update: added beacon contract support and fixed issue with proxy contracts
    
    * update: fixed .env

--- src/api/explorerV1.ts
+++ src/api/explorerV1.ts
@@ -14,6 +14,8 @@ export interface IVerifyContractData {
   isLoading: boolean;
   argsLoading: boolean;
   error: string;
+  tab: string;
+  fileList?: File[];
   language: number;
 }
 
@@ -27,32 +29,85 @@ export interface IVerifyContractDataSendData {
   constructorArguments: string;
   chainType: string;
   contractName: string;
+  fileList?: File[],
+  tab: string,
+
 }
 
 export const verifyContractCode = async (data: IVerifyContractDataSendData) => {
-  const response = await fetch(
-    `${process.env.REACT_APP_EXPLORER_V1_API_URL}codeVerification`,
-    {
-      method: "POST",
-      mode: "cors",
-      cache: "no-cache",
-      credentials: "same-origin",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      redirect: "follow",
-      referrerPolicy: "no-referrer",
-      body: JSON.stringify(data),
+  
+  if (data.tab === "Multiple Source Files") {
+    console.log("Handling multiple files");
+    const formData = new FormData();
+    data.fileList?.forEach(file=>{
+      formData.append(file.name, file);
+    });
+
+    for (const [k, v] of Object.entries(data)) {
+      if (k === "fileList") {
+        continue;
+      }
+
+      if (k === "language" && +v === 0) {
+        continue;
+      }
+      
+      if (k === "libraries") {
+        formData.append(k, v.join(","));
+      }
+      else {
+        formData.append(k, v);
+      }
     }
-  );
 
-  const body = await response.json();
+    const response = await  fetch(
+      `${process.env.REACT_APP_EXPLORER_V1_API_URL}codeVerification`,
+      {
+        method: "POST",
+        mode: "cors",
+        cache: "no-cache",
+        credentials: "same-origin",
+        redirect: "follow",
+        referrerPolicy: "no-referrer",
+        body: formData,
+      }
+    );
 
-  if (response.status !== 200) {
-    throw new Error(body?.message);
+    const body = await response.json();
+
+    console.log("Handling multiple files", body);
+
+    if (response.status !== 200) {
+      throw new Error(body?.message);
+    }
+
+    return body;
   }
+  else {
+    const response = await fetch(
+      `${process.env.REACT_APP_EXPLORER_V1_API_URL}codeVerification`,
+      {
+        method: "POST",
+        mode: "cors",
+        cache: "no-cache",
+        credentials: "same-origin",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        redirect: "follow",
+        referrerPolicy: "no-referrer",
+        body: JSON.stringify(data),
+      }
+    );
 
-  return body;
+    const body = await response.json();
+
+    if (response.status !== 200) {
+      throw new Error(body?.message);
+    }
+
+    return body;
+  }
 };
 
 export const loadSourceCode = async (address: string): Promise<ISourceCode> => {
@@ -85,9 +140,13 @@ export interface ISourceCode {
   optimizer: string;
   optimizerTimes: string;
   sourceCode: string;
+  supporting: any;
   libraries: string[];
   constructorArguments: string;
   chainType: string;
   contractName: string;
   abi?: AbiItem[];
+  proxyAddress?: string;
+  proxyDetails?: any;
+  proxy?: any;
 }
