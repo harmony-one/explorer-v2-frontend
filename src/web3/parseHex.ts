4d6b46945f9bdbf0cd471795df0ec82dca49e431
artemkolodko <temakolodko@gmail.com>
2022-04-05 13:30:39 +0100

    Improve parse text from hex

--- src/web3/parseHex.ts
+++ src/web3/parseHex.ts
@@ -3,13 +3,12 @@ import Web3 from "web3";
 const web3 = new Web3()
 
 export const parseHexToText = (hex: string) => {
-  let text = ''
   try {
-    text = web3.utils.hexToUtf8(hex)
+    const text = web3.utils.hexToUtf8(hex)
     // Allow only english letters and some symbols
     if (text && /^[A-Za-z0-9,.;\- ]*$/.test(text)) {
       return text
     }
   } catch (e) {}
-  return text
+  return ''
 }
