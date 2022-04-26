e21175729f233384bfeed4a2d5e819303b417af1
artemkolodko <temakolodko@gmail.com>
2022-04-26 21:11:38 +0100

    Use etherscan format to export transactions to CSV

--- src/pages/ExportData/export-utils.ts
+++ src/pages/ExportData/export-utils.ts
@@ -1,5 +1,9 @@
-const downloadBlob = (content: any, filename: string, contentType: string) => {
-  const blob = new Blob([content], { type: contentType });
+import { RelatedTransaction } from "../../types";
+import dayjs from "dayjs";
+import { calculateFee, calculateFeePriceUSD } from "../../utils/fee";
+
+const downloadBlob = (content: any, filename: string) => {
+  const blob = new Blob([content], { type: 'text/csv;charset=utf-8;' });
   const url = URL.createObjectURL(blob);
 
   // Create a link to download it
@@ -9,9 +13,43 @@ const downloadBlob = (content: any, filename: string, contentType: string) => {
   pom.click();
 }
 
-export const downloadCSV = (data: any[], filename: string) => {
-  const header= data.filter((_, index) => index === 0).map(item => Object.keys(item))
-  const body = data
+const mapRelatedTxToExport = (ownerAddress: string, tx: RelatedTransaction, onePrice: number) => {
+  const txDate = dayjs(tx.timestamp)
+
+  const convertValue = (value: string | number, n = 4) => {
+    const precision = 18
+    const bigIntValue = BigInt(value) / BigInt(10 ** (precision - n))
+    return parseInt(bigIntValue.toString()) / (10 ** n);
+  }
+
+  const isSender = ownerAddress === tx.from
+  return {
+    Txhash: tx.hash,
+    Blockno: tx.blockNumber,
+    UnixTimestamp: txDate.unix(),
+    DateTime: txDate.format('YYYY-MM-DD HH:MM:ss'),
+    From: tx.from,
+    To: tx.to,
+    ['Value_IN(ONE)']: convertValue(isSender ? '0': tx.value),
+    ['Value_OUT(ONE)']: convertValue(isSender ? tx.value : '0'),
+    [`CurrentValue @ $${onePrice}/ONE`]: convertValue(onePrice * +tx.value),
+    ['TxnFee(ONE)']: calculateFee(tx.gas, tx.gasPrice),
+    ['TxnFee(USD)']: calculateFeePriceUSD(tx.gas, tx.gasPrice, onePrice),
+    Method: tx.input.slice(0, 10)
+  }
+}
+
+export interface IDownloadCsvParams {
+  address: string
+  txs: RelatedTransaction[]
+  onePrice: number
+}
+
+export const downloadCSV = (params: IDownloadCsvParams, filename: string) => {
+  const { address, txs, onePrice} = params
+  const mappedTxs = txs.map((tx) => mapRelatedTxToExport(address, tx, onePrice))
+  const header = mappedTxs.filter((_, index) => index === 0).map(item => Object.keys(item))
+  const body = mappedTxs
     .map((item) => Object.values(item))
 
   const csv = [...header, ...body]
@@ -23,5 +61,5 @@ export const downloadCSV = (data: any[], filename: string) => {
         .join(', ')  // comma-separated
     ).join('\r\n');  // rows starting on new lines
 
-  downloadBlob(csv, filename, 'text/csv;charset=utf-8;')
+  downloadBlob(csv, filename)
 }
--- src/pages/ExportData/index.tsx
+++ src/pages/ExportData/index.tsx
@@ -8,6 +8,7 @@ import { getRelatedTransactionsByType } from "../../api/client";
 import { downloadCSV } from "./export-utils";
 import dayjs from "dayjs";
 import { toaster } from "../../App";
+import { useONEExchangeRate } from "../../hooks/useONEExchangeRate";
 
 const IconError = styled(StatusCritical)`
   margin-right: 5px;
@@ -39,6 +40,7 @@ export const ExportData = () => {
   const initialDateFrom = dayjs().startOf('month').format(dateFormat)
   const initialDateTo = dayjs().format(dateFormat)
 
+  const { lastPrice } = useONEExchangeRate();
   const [isDownloading, setIsDownloading] = useState(false)
   const [dateFrom, setDateFrom] = useState(initialDateFrom)
   const [dateTo, setDateTo] = useState(initialDateTo)
@@ -80,13 +82,13 @@ export const ExportData = () => {
   const onDownloadClicked = async () => {
     try {
       setIsDownloading(true)
-      const data = await getRelatedTransactionsByType([
+      const txs = await getRelatedTransactionsByType([
         0,
         address,
         'transaction',
         filter,
       ]);
-      downloadCSV(data, `export_${address}.csv`)
+      downloadCSV({ address, txs, onePrice: lastPrice }, `export_${address}.csv`)
     } catch (e) {
       console.error('Error on download:', (e as Error).message)
       showErrorNotification()
