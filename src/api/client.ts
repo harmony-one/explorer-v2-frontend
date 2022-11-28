95fe09992a1ae60b88a4cdc9822d25d4f49c6f27
jenya <hypnagonia>
2022-11-28 14:20:23 +0400

    contract shard to config

--- src/api/client.ts
+++ src/api/client.ts
@@ -25,7 +25,7 @@ import { eth_traceTransaction } from './rpc'
 
 // TODO: hardcode
 let pairCache: { [pair: string]: IPairPrice } = {};
-const contractShardID = 1
+const contractShardID = +process.env.REACT_APP_CONTRACT_SHARD || 0
 
 setInterval(() => {
   pairCache = {};
